7b62ce215fc4e05bc0be424eaf4548776a7506b8
Luis Morais <138785109+luismoraiss@users.noreply.github.com>
2024-08-19 19:08:21 -0300

    qwde

---
+++ programa.js
@@ -0,0 +1,21 @@
+document.getElementById('calculate').addEventListener('click', function(){
+    debugger
+    
+    const av1 =document.getElementById('av1').value
+    const av2 =document.getElementById('av2').value
+    const avt = document.getElementById('avt').value
+
+    if(av1 > 10 || av1 < 0 || av2 > 10 || av2 < 0 || avt > 10 || avt <0){
+    alert("Digite um valor de 0 a 10")
+    location.reload()
+
+    }
+
+    const total = ((av1 *25) + (av2*25) + (avt*50)) / 100
+   
+    document.getElementById('total').innerHTML = (total.toFixed(2))
+});
+
+
+
+
